5086e3fc0c9c9a208cfcc183431cb5f5403d1b5c
cem <cemyilmaztech@gmail.com>
2024-09-11 11:24:28 +0300

    TeacherDetails

--- src/pages/TeacherDetails.jsx
+++ src/pages/TeacherDetails.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React from "react";
+import { useLocation } from "react-router-dom";
 
 const TeacherDetails = () => {
-  const{state:person}=useLocation();
+  const { state: person } = useLocation();
   return (
-   
-    <div>TeacherDetails</div>
-  )
-}
+    <div className="text-center">
+      <img src={`https://api.dicebear.com/9.x/avataaars/svg?seed=${person.name}`} alt="img" width="400px"/>
+      <h5>{person.name}</h5>
+      <h6>{person.username} </h6>
+      <h4>{person.phone} </h4>
+    </div>
+  );
+};
 
-export default TeacherDetails
+export default TeacherDetails;
